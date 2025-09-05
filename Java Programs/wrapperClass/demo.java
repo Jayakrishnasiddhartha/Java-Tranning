7debbd7142477a802aee89f510e5e2a5e9b324ca
Jaya Krishna Siddhartha <jayakrishnasiddhartha.n@gmail.com>
2025-09-05 10:38:29 +0000

    Proper naming convention

--- Java Programs/wrapperClass/demo.java
+++ Java Programs/wrapperClass/demo.java
@@ -8,10 +8,6 @@ public class demo{
         int num2 = num1; //unboxing
         System.out.println(num2);
 
-
-
-
-
         String str = "123";
         // System.out.println(str*2);
         int num3 = Integer.parseInt(str);
